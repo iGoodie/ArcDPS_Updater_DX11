e033b3ba0e79f124e32fbb816827852f624cfe35
Anılcan Metinyurt <igoodie@programmer.net>
2022-09-19 00:57:50 +0300

    feat: create base ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import * as TauriAPI from "@tauri-apps/api/tauri";
-import "./App.css";
+import UpdateIcon from "@/assets/icons/update.svg";
+import "@/style/main.scss";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await TauriAPI.invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+    <div id="app">
+      <div className="page">
+        <div className="app-title">
+          <img src={UpdateIcon} height={40} />
+          <h1>Arcdps Updater</h1>
+          <span className="divider" />
+          <h2>For DX11 Update</h2>
         </div>
       </div>
-      <p>{greetMsg}</p>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./style.css";
 import "@/utils/appdata.util.ts";
 
 const rootElement = document.getElementById("root") as HTMLElement;
